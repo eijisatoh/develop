465cbfe58a469b97c6db528690a1d9a49ca4e4cb
sato_eiji_gn <sato_eiji_gn@cyberagent.co.jp>
2013-04-08 17:07:10 +0900

    test

---
+++ testGit/src/jp/test/Test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jp.test;
+
+/**
+ * @author B03195
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
